792dda7ca134854a23b030ff004c2d101953433b
pedro-valdez <pedro.valdez.dev@protonmail.com>
2023-04-18 17:52:58 -0500

    Added buttons for respective functionalities
    Everything that is done through the keyboard can be done with buttons.

--- components/GolSketch/structure.tsx
+++ components/GolSketch/structure.tsx
@@ -3,12 +3,25 @@ import { game } from "."
 import { squareArrayApply } from "@/lib/gol"
 import settings from "@/lib/settings"
 
+let resetButton
+let loopButton
+let cycleButton
 export const setup = (p5: p5Types, canvasParent: Element) => {
 	p5.createCanvas(512, 512).parent(canvasParent)
 	settings.scale = p5.width / game.size
 
 	p5.noStroke()
 	p5.frameRate(8)
+
+	// Element creation
+	resetButton = p5.createButton('Reset')
+	resetButton.mousePressed(() => game.reset())
+
+	loopButton = p5.createButton('Loop')
+	loopButton.mousePressed(() => settings.isCycling = !settings.isCycling)
+
+	cycleButton = p5.createButton('Next')
+	cycleButton.mousePressed(() => game.cycle())
 }
 
 export const draw = (p5: p5Types) => {
