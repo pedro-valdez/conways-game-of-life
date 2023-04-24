5d85a55a60449ffce60f81b8dcdbdde620554ebb
pedro-valdez <pedro.valdez.dev@protonmail.com>
2023-04-23 21:07:26 -0500

    Moved heading styling to base layer

--- components/GolSection/Title.tsx
+++ components/GolSection/Title.tsx
@@ -1,6 +1,6 @@
 export default function Title() {
 	return (
-		<h1 className="font-black text-4xl text-emerald-500">
+		<h1 className="text-emerald-500">
 			Conway's<br className="sm:hidden"/> Game of Life
 		</h1>
 	)
